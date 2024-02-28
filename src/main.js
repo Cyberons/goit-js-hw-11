a851c2e1d2c4534d6975ab5552692b370def132e
Cyberons <platon.pustovoyt@gmail.com>
2024-02-28 09:31:28 +0200

    Function for interface elements

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { fetchData } from "./js/pixabay-api.js";
+import { renderMurcup } from "./js/render-functions.js";
